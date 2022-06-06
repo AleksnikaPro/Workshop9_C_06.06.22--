9abfc43782d217d32fdd60c7fb7d56611a55b30b
Aleksandra Popova <alexnika@bk.ru>
2022-06-06 16:39:54 +0300

    added  task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,52 @@
+﻿//2.Показать натуральные числа от 1 до N, N задано. Рекурсия.
+
+
+Console.WriteLine("N = ");
+int number = int.Parse(Console.ReadLine());
+void Recursion(int n)
+{
+    if (n < 1)return;
+    {
+        Recursion(n - 1);
+        Console.Write(n + " ");
+    }
+}
+Recursion(number);
+=============================================================
+
+
+void Recursion(int n, int i)
+{
+    if (i < n) Console.Write($"{i}, ");
+    if (i == n) Console.Write($"{i}");
+    if (i > n) return;
+    Recursion(n, i + 1);     
+    
+}
+
+int n = 20;
+int i = 1;
+Recursion(n, i);
+==============================================================
+
+void PrintNumber(int n)
+{
+    n--;
+    if (n == 0) return;
+    PrintNumber(n);   
+    Console.Write(n + " ");
+}
+
+int n = 20;
+PrintNumber(n); 
+=============================================================
+
+void ShowNumbers(int Numbers)
+{
+ 
+    if (Numbers < 1) return;
+    ShowNumbers(Numbers - 1);
+    Console.Write(Numbers + " ");
+}
+
+ShowNumbers(15);
